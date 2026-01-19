3a26176c7b201bb6ba4f5f0cdec9fab2001c23cc
Stephen Davis <stephenedavis17@gmail.com>
2026-01-18 20:47:31 -0500

    more build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production'
+const repoName = 'apartment-gallery'
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
   images: {
     unoptimized: true,
   }
